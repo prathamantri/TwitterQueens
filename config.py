3a52beeba9a54dda2592366e9585c4d0e46a9f4c
cbonfantep <41312372+cbonfantep@users.noreply.github.com>
2019-06-25 20:03:39 -0700

    Config for code

---
+++ config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+api_key = 'S85XMxe2E33xjJg0JZqhPeiFJ'
+
+api_secret_key = '8yrEfLMnQ0K4TknquwYrgEgl5gpuGrnraLAlkvFCVyl5YIQNmw'
+
+access_token = '2222538177-ydVhqc7UFKXj5iJW8l0JEcwZ56ILKmZM72wUmzD'
+
+access_token_secret = 'uZy3zrbdO4kZ1ePrtlxFEusNvmcvF9ryoKlgkd0Nr8ioo'
+
